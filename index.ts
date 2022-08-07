029ede7bc4977d16ef93c995dc935ef6447d4441
Caleb Evans <caleb@calebevans.me>
2022-08-07 14:16:47 -0700

    Add code comment linking to source of watch code

--- index.ts
+++ index.ts
@@ -2,8 +2,10 @@ import glob from 'glob';
 import path from 'path';
 import { PluginHooks } from 'rollup';
 
+// Watch any arbitrary project files for changes, such as static assets
+// (source:
+// <https://github.com/rollup/rollup/issues/3414#issuecomment-751699335>)
 function watchGlobs(globs: string[]): Partial<PluginHooks> {
-
   return {
     buildStart() {
       for (const item of globs) {
@@ -13,7 +15,6 @@ function watchGlobs(globs: string[]): Partial<PluginHooks> {
       }
     }
   }
-
 }
 
 export default watchGlobs;
